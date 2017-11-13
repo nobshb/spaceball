9f7bb8884db9fbfaf5c6beb81af60a5628bf121e
Nobo Bhowmik <nobonita.bhowmik@outlook.com>
2017-11-12 21:55:05 -0500

    FirstDraft

---
+++ SpaceBall/Assets/Scripts/BoundaryDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoundaryDestroy : MonoBehaviour {
+
+	void OnTriggerExit(Collider other)
+	{
+		Destroy(other.gameObject);
+		//SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+	}
+}
